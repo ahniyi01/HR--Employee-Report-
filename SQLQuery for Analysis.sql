90901b319f9d71e148fb0b597b1bcaaeac95c7c7
Ahmed Adewale <ahniyi01@yahoo.com>
2024-01-30 11:27:27 +0100

    Update SQLQuery for Analysis.sql

--- SQLQuery for Analysis.sql
+++ SQLQuery for Analysis.sql
@@ -41,7 +41,31 @@ GROUP BY
         WHEN age >= 55 AND age <= 64 THEN '55-64'
         ELSE '65+'
     END;
-	
+
+-- To get the age distribution by Department 
+SELECT
+    CASE
+        WHEN age >= 18 AND age <= 24 THEN '18-24'
+        WHEN age >= 25 AND age <= 34 THEN '25-34'
+        WHEN age >= 35 AND age <= 44 THEN '35-44'
+        WHEN age >= 45 AND age <= 54 THEN '45-54'
+        WHEN age >= 55 AND age <= 64 THEN '55-64'
+        ELSE '65+'
+    END AS age_group, gender, 
+    COUNT(*) AS count
+FROM [Tutorial].[dbo].[Human_Resources]
+WHERE age >= 18 AND termdate is NULL
+GROUP BY
+    CASE
+        WHEN age >= 18 AND age <= 24 THEN '18-24'
+        WHEN age >= 25 AND age <= 34 THEN '25-34'
+        WHEN age >= 35 AND age <= 44 THEN '35-44'
+        WHEN age >= 45 AND age <= 54 THEN '45-54'
+        WHEN age >= 55 AND age <= 64 THEN '55-64'
+        ELSE '65+'
+    END, gender
+	ORDER BY age_group;
+
 
 -- 4. How many employees work at headquarters versus remote locations?
 SELECT location, COUNT(*) AS count
